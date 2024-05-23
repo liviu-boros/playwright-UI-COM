8e561b4d616a9f562d79085418031c545a9d6029
liviu-boros <boros.liviu@gmail.com>
2024-05-23 20:14:49 +0300

    updated Button and NavBar tests

--- components/NavItem.ts
+++ components/NavItem.ts
@@ -1,6 +1,6 @@
 import { Locator, Page } from "@playwright/test";
 
-export const NavItem = (anchor: Locator | Page, text: string) => {
+export const NavItem = (anchor: Locator | Page, text: string): Locator => {
   let locator: Locator;
 
   locator = anchor.locator(`a.mat-list-item :text-is("${text}")`);
